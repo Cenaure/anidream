bbc9f99d4f54cebe946716cbe8f929bbbe2450d1
Cenaure <discordjahysama@gmail.com>
2026-05-04 00:33:44 +0200

    Railway server

--- src/env/dev.env.ts
+++ src/env/dev.env.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api', //rust-server
+  apiUrl: 'https://rust-server-production-0c70.up.railway.app/api', //rust-server
   websocketUrl: "ws://localhost:8080/ws",
 };
